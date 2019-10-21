d3ef1264a969e52b6b19e5f98e4adacfe3c78ecb
Scott Daniels <sdaniels6363@gmail.com>
2019-10-21 16:43:06 -0400

    inital commit, inserted functions to update burgers

--- config/orm.js
+++ config/orm.js
@@ -2,22 +2,30 @@ var connection = require("./connection.js") // import mysql connection
 
 var orm = {
     // retrieve all results from the database.
-    selectAll: function(){
-        var query = "SELECT * FROM burgers"
+    selectAll: function(table){
+        var query = `SELECT * FROM ${table}`
         connection.query(query, function(err, data){
             if (err) throw err;
-
-            
+            return data;
         });
     },
     
     // add a new burger to the database.
-    insertOne: function(){
-
+    insertOne: function(table,colName,value){
+        var query = `INSERT INTO ${table} (${colName}) VALUES ${value}`
+        connection.query(query, [burgerName], function(err, data){
+            if (err) throw err;
+            return data;
+        })
     },
 
     // update the devoured status of a burger in the database.
-    updateOne: function(){
+    updateOne: function(table,colToUpdate,boolean,whereField,value){
+        var query = `UPDATE ${table} SET ${colToUpdate} = ${boolean} WHERE ${whereField}=${value}`;
+        connection.query(query, [burgerName], function(err, data){
+            if (err) throw err;
+            return data;
+        });
 
     }
 }
