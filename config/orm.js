4676fdc55f491f7814baab051481e2651ed3caf1
Scott Daniels <sdaniels6363@gmail.com>
2019-10-21 18:33:47 -0400

    converted return into pass back to callback

--- config/orm.js
+++ config/orm.js
@@ -2,29 +2,29 @@ var connection = require("./connection.js") // import mysql connection
 
 var orm = {
     // retrieve all results from the database.
-    selectAll: function(table){
+    selectAll: function(table, cb){
         var query = `SELECT * FROM ${table}`
         connection.query(query, function(err, data){
             if (err) throw err;
-            return data;
+            cb(data);
         });
     },
     
     // add a new burger to the database.
-    insertOne: function(table,colName,value){
+    insertOne: function(table,colName,value , cb){
         var query = `INSERT INTO ${table} (${colName}) VALUES ${value}`
         connection.query(query, [burgerName], function(err, data){
             if (err) throw err;
-            return data;
+            cb(data);
         })
     },
 
     // update the devoured status of a burger in the database.
-    updateOne: function(table,colToUpdate,boolean,whereField,value){
+    updateOne: function(table,colToUpdate,boolean,whereField,value , cb){
         var query = `UPDATE ${table} SET ${colToUpdate} = ${boolean} WHERE ${whereField}=${value}`;
         connection.query(query, [burgerName], function(err, data){
             if (err) throw err;
-            return data;
+            cb(data);
         });
 
     }
