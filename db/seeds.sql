345cd3494330be6b3547b6141d7225cfd2a0b097
Scott Daniels <sdaniels6363@gmail.com>
2019-10-20 20:52:54 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Baconater');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Double Quarter Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Sourdough Jack');
+INSERT INTO burgers (burger_name) VALUES ('Frisco Burger');
