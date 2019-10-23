7c908abfaebdaf8acf0deccba162e311e3231e50
Scott Daniels <sdaniels6363@gmail.com>
2019-10-22 22:16:52 -0400

    made devoured update based on existing state

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -23,7 +23,7 @@ router.post("/api/burgers", function (req, res) {
 });
 
 router.put("/api/burgers/:id", function (req, res) {
-  burger.updateDevoured("devoured", "true", "id", req.params.id, function (result) {
+  burger.updateDevoured("devoured", req.body.devoured, "id", req.params.id, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
